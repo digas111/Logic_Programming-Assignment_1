ba36adb5fb5795d5ae3571e4548e4a13655847fa
digas111 <didiogomorais@hotmail.com>
2018-11-21 14:40:01 +0000

    funciona qualquer coisinha

--- trabalho.pl
+++ trabalho.pl
@@ -9,13 +9,18 @@ coefficient(K):-number(K).
 
 
 monomial(X):-pvar(X),!.
-% monomial(-X):-pvar(X),!.
 monomial(N):-number(N),!.
 monomial(X):-power(X),!.
 monomial(K*X):-coefficient(K),power(X),!.
 
+polynomial(M):-monomial(M),!.
+polynomial(P+M):-monomial(M),polynomial(P),!.
 
-poly2list(M+P,[M|R]):-monomial(M), poly2list(P,R),!.
-poly2list(M-P,[M|R]):-monomial(M),poly2list(P,R),!.
+
+% poly2list(M+P,[M|R]):-monomial(M), poly2list(P,R),!.
+% poly2list(M-P,[M|R]):-monomial(M),poly2list(P,R),!.
 % poly2list(M,[M]):-monomial(M),!.
+
+poly2list(M+P,[P|R]):-poly2list(M,R),!.
+poly2list(M-P,[P|R]):-poly2list(M,R),!.
 poly2list(M,[M]).
